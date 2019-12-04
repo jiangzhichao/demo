8b0352810c435a30f485a2172d4d4f9f77e7588b
jiangzhichao <365703314@qq.com>
2019-12-04 11:13:24 +0800

    Update socket.js

--- webpack/socket.js
+++ webpack/socket.js
@@ -1,1 +1,26 @@
+let keyCodeObj = {};
 
+this.addEventListener('message', msg => {
+    keyCodeObj = msg.data.keyCodeObj || {};
+
+    if (msg.data.begin) {
+        const socket = io('', {path: '/ws'});
+
+        socket.on('connect', () => {
+            socket
+                .emit('authenticate', { token: msg.data.cookie })
+                .on('authenticated', () => {
+                    socket.emit('login', msg.data.user);
+                })
+                .on('unauthorized', _msg => {
+                    console.log(_msg);
+                })
+                .on('loop', res => {
+                    socket.emit('ok', keyCodeObj);
+
+                    this.postMessage(res);
+                });
+        });
+    }
+
+});
